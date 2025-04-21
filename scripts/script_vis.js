7ce2f1304dfca5d189e78019ea8747438498899d
Salomelanderos11 <146784384+Salomelanderos11@users.noreply.github.com>
2025-04-20 21:15:29 -0700

    Update script_vis.js

--- scripts/script_vis.js
+++ scripts/script_vis.js
@@ -8,7 +8,7 @@ var valor= 0;
 
 window.onload = function() { 
     var value = objetoRecuperado.nombre; 
-    var ruta= "media/"+(value)+"/"+(value)+".jpg"
+    var ruta= "/media/"+(value)+"/"+(value)+".jpg"
     miImagen.src=ruta;
     }
 /*Extraer valor compartido del localstorage al cargar la pagina y establecer la imagen inicial*/
@@ -30,7 +30,7 @@ function sig_right() {
     if (valor > 4) {  
         valor = 0;
     }
-    var texto = valor === 0 ? `media/${objetoRecuperado.nombre}/${objetoRecuperado.nombre}.jpg` : `media/${objetoRecuperado.nombre}/${objetoRecuperado.nombre}.${valor}.jpg`;
+    var texto = valor === 0 ? `/media/${objetoRecuperado.nombre}/${objetoRecuperado.nombre}.jpg` : `/media/${objetoRecuperado.nombre}/${objetoRecuperado.nombre}.${valor}.jpg`;
     miImagen.src = texto;
 }
 
@@ -39,7 +39,7 @@ function sig_left() {
     if (valor < 0) {
         valor = 4;
     }
-    var texto = valor === 0 ? `media/${objetoRecuperado.nombre}/${objetoRecuperado.nombre}.jpg` : `media/${objetoRecuperado.nombre}/${objetoRecuperado.nombre}.${valor}.jpg`;
+    var texto = valor === 0 ? `/media/${objetoRecuperado.nombre}/${objetoRecuperado.nombre}.jpg` : `/media/${objetoRecuperado.nombre}/${objetoRecuperado.nombre}.${valor}.jpg`;
     miImagen.src = texto;
 }
 
